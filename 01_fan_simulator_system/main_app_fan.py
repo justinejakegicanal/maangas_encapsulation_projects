0baa2a0a964608d0602c13d261ddb45422986186
Jake <justinejake16@gmail.com>
2026-06-02 03:46:48 +0800

    feat(fan): instantiate dual fan objects with operational attributes

--- 01_fan_simulator_system/main_app_fan.py
+++ 01_fan_simulator_system/main_app_fan.py
@@ -6,3 +6,13 @@ class FanSimulatorApplication:
     @staticmethod
     def clear_screen():
         os.system('cls' if os.name == 'nt' else 'clear')
+
+    def run_simulation(self):
+        self.clear_screen()
+        
+        fan_unit_one = Fan(speed=Fan.FAST, radius=10.0, color="yellow", on=True)
+        fan_unit_two = Fan(speed=Fan.MEDIUM, radius=5.0, color="blue", on=False)
+
+if __name__ == "__main__":
+    app_engine = FanSimulatorApplication()
+    app_engine.run_simulation()
