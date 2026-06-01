74f9534112f83766b29f9c60c6825434eafd234f
Jake <justinejake16@gmail.com>
2026-06-02 02:44:22 +0800

    feat(fan): define base class architecture and speed constants

---
+++ 01_fan_simulator_system/fan_logic.py
@@ -0,0 +1,10 @@
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.on = on
