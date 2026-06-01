cdaa9763435f98d06a41d3749e7e31eeba115140
Jake <justinejake16@gmail.com>
2026-06-02 03:22:21 +0800

    feat(fan): add setters with validation rules para umangas

--- 01_fan_simulator_system/fan_logic.py
+++ 01_fan_simulator_system/fan_logic.py
@@ -8,7 +8,7 @@ class Fan:
         self.__radius = float(radius)
         self.__color = str(color)
         self.__on = bool(on)
-        
+
     def get_speed(self):
         return self.__speed
 
@@ -20,5 +20,26 @@ class Fan:
 
     def get_on(self):
         return self.__on
+    
+    def set_speed(self, speed):
+        if speed in [Fan.SLOW, Fan.MEDIUM, Fan.FAST]:
+            self.__speed = speed
+        else:
+            raise ValueError("Invalid fan speed level index.")
+
+    def set_radius(self, radius):
+        if radius > 0:
+            self.__radius = float(radius)
+        else:
+            raise ValueError("Fan blade radius must be a positive scalar attribute.")
+
+    def set_color(self, color):
+        if color.strip():
+            self.__color = str(color)
+        else:
+            raise ValueError("Fan aesthetic color property cannot be empty.")
+
+    def set_on(self, on_status):
+        self.__on = bool(on_status)
 
     
