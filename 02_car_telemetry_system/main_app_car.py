945d9fcbaea4abe9453cb5a993b4018f8681b6e4
Jake <justinejake16@gmail.com>
2026-06-02 10:31:28 +0800

    feat(car): instantiate test vehicle object within main application loop

--- 02_car_telemetry_system/main_app_car.py
+++ 02_car_telemetry_system/main_app_car.py
@@ -6,3 +6,11 @@ class CarTelemetryApplication:
     @staticmethod
     def clear_screen():
         os.system('cls' if os.name == 'nt' else 'clear')
+
+    def run_simulation(self):
+        self.clear_screen()
+        test_car = Car(2026, "Maangas GT")
+
+if __name__ == "__main__":
+    app_engine = CarTelemetryApplication()
+    app_engine.run_simulation()
