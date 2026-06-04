180ddacfe20bee0d0a5077217d413b3b63d9662a
Jake <justinejake16@gmail.com>
2026-06-04 15:41:07 +0800

    feat(car): define strictly encapsulated telemetry attributes inside constructor

--- 02_car_telemetry_system/car_logic.py
+++ 02_car_telemetry_system/car_logic.py
@@ -1,3 +1,7 @@
 class Car:
     def __init__(self, brand, model, year):
-        pass
+        self.__brand = str(brand)
+        self.__model = str(model)
+        self.__year = int(year)
+        self.__current_speed = 0
+        self.__is_engine_on = False
