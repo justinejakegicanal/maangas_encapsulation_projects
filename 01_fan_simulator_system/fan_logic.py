17974cbf97363aa851ac7c7075d4f4c2351a36fd
Jake <justinejake16@gmail.com>
2026-06-04 12:41:49 +0800

    feat(fan): implement secure getters and conditional control setter mechanics

--- 01_fan_simulator_system/fan_logic.py
+++ 01_fan_simulator_system/fan_logic.py
@@ -2,4 +2,20 @@ class Fan:
     def __init__(self, brand, max_speed):
         self.__brand = str(brand)
         self.__max_speed = int(max_speed)
-        self.__current_speed = 0    
+        self.__current_speed = 0
+
+    def get_brand(self):
+        return self.__brand
+
+    def get_max_speed(self):
+        return self.__max_speed
+
+    def get_current_speed(self):
+        return self.__current_speed
+
+    def set_speed(self, speed):
+        new_speed = int(speed)
+        if 0 <= new_speed <= self.__max_speed:
+            self.__current_speed = new_speed
+            return True
+        return False
