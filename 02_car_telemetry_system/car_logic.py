c32470de8bd4a2fdfa43c191c52999e37edef297
Jake <justinejake16@gmail.com>
2026-06-04 15:45:01 +0800

    refactor(car): remove development comments for clean production codebase

--- 02_car_telemetry_system/car_logic.py
+++ 02_car_telemetry_system/car_logic.py
@@ -5,3 +5,43 @@ class Car:
         self.__year = int(year)
         self.__current_speed = 0
         self.__is_engine_on = False
+
+    def get_brand(self):
+        return self.__brand
+
+    def get_model(self):
+        return self.__model
+
+    def get_year(self):
+        return self.__year
+
+    def get_current_speed(self):
+        return self.__current_speed
+
+    def is_engine_on(self):
+        return self.__is_engine_on
+
+    def toggle_engine(self):
+        if self.__is_engine_on:
+            self.__is_engine_on = False
+            self.__current_speed = 0
+            return "Engine Stopped"
+        else:
+            self.__is_engine_on = True
+            return "Engine Started"
+
+    def accelerate(self, increment):
+        if not self.__is_engine_on:
+            return False
+        
+        self.__current_speed += int(increment)
+        return True
+
+    def brake(self, decrement):
+        if not self.__is_engine_on:
+            return False
+            
+        self.__current_speed -= int(decrement)
+        if self.__current_speed < 0:
+            self.__current_speed = 0
+        return True
