20267f4153788424309c75139ad1c076aa61c6e4
Jake <justinejake16@gmail.com>
2026-06-04 12:31:02 +0800

    feat(fan): define strictly encapsulated state attributes inside constructor

--- 01_fan_simulator_system/fan_logic.py
+++ 01_fan_simulator_system/fan_logic.py
@@ -1,3 +1,5 @@
 class Fan:
     def __init__(self, brand, max_speed):
-        pass
+        self.__brand = str(brand)
+        self.__max_speed = int(max_speed)
+        self.__current_speed = 0    
