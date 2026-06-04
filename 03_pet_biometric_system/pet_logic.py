8f4617aa897343ab7ccc44cc761d38c17e6d36f7
Jake <justinejake16@gmail.com>
2026-06-04 11:31:55 +0800

    feat(pet): implement secure getter methods for private properties

--- 03_pet_biometric_system/pet_logic.py
+++ 03_pet_biometric_system/pet_logic.py
@@ -3,3 +3,12 @@ class Pet:
         self.__name = str(name)
         self.__animal_type = str(animal_type)
         self.__age = int(age)
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
